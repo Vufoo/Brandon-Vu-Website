a498a48d8151c61162fab48bdac082a5ca700bfb
Brandon Vu <61858661+Vufoo@users.noreply.github.com>
2024-08-15 11:29:29 -0500

    Hover update

--- script.js
+++ script.js
@@ -4,3 +4,24 @@ function toggleMenu() {
     menu.classList.toggle("open");
     icon.classList.toggle("open");
 }
+
+
+document.querySelectorAll('.details-container').forEach(container => {
+    container.addEventListener('click', () => {
+        container.classList.toggle('active');
+    });
+});
+
+// For projects
+document.querySelectorAll('.project').forEach(project => {
+    project.addEventListener('click', () => {
+        project.classList.toggle('active');
+    });
+});
+
+// For courses
+document.querySelectorAll('.course').forEach(course => {
+    course.addEventListener('click', () => {
+        course.classList.toggle('active');
+    });
+});
